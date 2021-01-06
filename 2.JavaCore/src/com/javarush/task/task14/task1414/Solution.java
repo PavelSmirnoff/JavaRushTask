d38a4693c5ee69359ccd63fabc2b2430b38430ad
Pavel Smirnov <pavelsmirnov@Smirnoff-MB13.local>
2021-01-06 15:56:54 +0300

    Movi Factory and Cleaning centre Solved

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task14.task1414;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+MovieFactory
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //ввести с консоли несколько ключей (строк), пункт 7
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String keyMovie = null;
+        Movie movie;
+        while ((keyMovie = bufferedReader.readLine()) != null) {
+            movie = MovieFactory.getMovie(keyMovie);
+            if (movie == null) {
+                return;
+            } else {
+                System.out.println(movie.getClass().getSimpleName());
+            }
+        }
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            } else if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            } else if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
+
+    //Напишите тут ваши классы, пункт 3
+}
