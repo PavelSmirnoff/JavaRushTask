304a51afc7e32a842b472540eca1bc4ff80d8c51
Pavel Smirnov <pavelsmirnov@Smirnoff-MB13.local>
2021-01-09 17:06:37 +0300

    Перегрузка конструкторов

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution() {
+    }
+
+    SubSolution(double i) {
+        super(i);
+    }
+
+    protected SubSolution(short i) {
+        super(i);
+    }
+}
