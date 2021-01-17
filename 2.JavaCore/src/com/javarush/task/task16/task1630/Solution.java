5270ece5f16d1f0bc3a72f280f8d65f4ae9ad416
Pavel Smirnov <pavelsmirnov@Smirnoff-MB13.local>
2021-01-17 18:59:33 +0300

    Thread synchronized task

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -0,0 +1,76 @@
+package com.javarush.task.task16.task1630;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Последовательный вывод файлов
+*/
+
+public class Solution {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    //add your code here - добавьте код тут
+    static {
+        try (BufferedReader strFileName = new BufferedReader(new InputStreamReader(System.in));) {
+            firstFileName = strFileName.readLine();
+            secondFileName = strFileName.readLine();
+        } catch (IOException ignored) {
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        f.join();
+        System.out.println(f.getFileContent());
+    }
+
+    public interface ReadFileInterface {
+        void setFileName(String fullFileName);
+
+        String getFileContent();
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        String fileName;
+        String fileContent = "";
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fileName = fullFileName;
+        }
+
+        @Override
+        public void run() {
+            String line;
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                while ((line = br.readLine()) != null) {
+                    fileContent += line + " ";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public String getFileContent() {
+            return fileContent;
+        }
+    }
+}
