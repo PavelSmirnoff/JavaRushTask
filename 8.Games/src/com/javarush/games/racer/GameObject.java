b065ea5b515b4ba5c8ab8cc5c38da1e3210e8250
Pavel Smirnov <pavelsmirnov@Smirnoff-MB13.local>
2021-01-18 20:43:08 +0300

    RacerGame 10 from 28 task Solved

---
+++ 8.Games/src/com/javarush/games/racer/GameObject.java
@@ -0,0 +1,28 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+public class GameObject {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public int[][] matrix;
+
+    public GameObject(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+        width = this.matrix[0].length;
+        height = this.matrix.length;
+    }
+
+    public void draw(Game game) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                game.setCellColor(x + j, y + i, Color.values()[matrix[i][j]]);
+            }
+        }
+    }
+
+}
--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -7,6 +7,8 @@ public class RacerGame extends Game {
     public static final int HEIGHT = 64;
     public static final int CENTER_X = WIDTH / 2;
     public static final int ROADSIDE_WIDTH = 14;
+    private RoadMarking roadMarking;
+    private PlayerCar player;
 
     @Override
     public void initialize() {
@@ -16,11 +18,22 @@ public class RacerGame extends Game {
     }
 
     private void createGame() {
+        setTurnTimer(40);
+        roadMarking = new RoadMarking();
+        player = new PlayerCar();
         drawScene();
     }
 
     private void drawScene() {
         drawField();
+        roadMarking.draw(this);
+        player.draw(this);
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x<0||x>=WIDTH||y<0||y>=HEIGHT) return;
+        super.setCellColor(x, y, color);
     }
 
     private void drawField() {
@@ -37,6 +50,25 @@ public class RacerGame extends Game {
                 setCellColor(x, y, color);
             }
         }
+    }
+
+    @Override
+    public void onTurn(int step) {
+        moveAll();
+        drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key){
+            case RIGHT: player.setDirection(Direction.RIGHT); break;
+            case LEFT: player.setDirection(Direction.LEFT); break;
+        }
+    }
+
+    private void moveAll(){
+        roadMarking.move(player.speed);
+        player.move();
     }
 
 }
