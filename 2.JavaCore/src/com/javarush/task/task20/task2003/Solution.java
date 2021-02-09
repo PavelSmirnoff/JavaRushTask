c4106c70b098c5f6721a985f1eacfffa2b925978
Pavel Smirnov <smirnov@0x55.ru>
2021-02-09 19:48:45 +0300

    Знакомство с properties

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+/* 
+Знакомство с properties
+*/
+
+public class Solution {
+
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+    public static void save(OutputStream outputStream) throws Exception {
+        //напишите тут ваш код
+        Properties p = new Properties();
+        for (Map.Entry k : runtimeStorage.entrySet()) {
+            p.setProperty(k.getKey().toString(),k.getValue().toString());
+        }
+        p.store(outputStream,"runtimeStorage");
+    }
+
+    public static void load(InputStream inputStream) throws IOException {
+        //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        runtimeStorage = properties.entrySet().stream().collect(Collectors.toMap(k -> k.getKey().toString(), v -> v.getValue().toString()));
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {
+            load(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(runtimeStorage);
+    }
+}
