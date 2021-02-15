2174f382dd2a7a3ebd5b24249c55bd460a249198
Pavel Smirnov <smirnov@0x55.ru>
2021-02-15 21:37:25 +0300

    Сериализация под запретом

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Сериализация под запретом
+*/
+
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+    }
+
+    private void writeObject(java.io.ObjectOutputStream out) throws NotSerializableException {
+        throw new NotSerializableException();
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws NotSerializableException{
+        throw new NotSerializableException();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
