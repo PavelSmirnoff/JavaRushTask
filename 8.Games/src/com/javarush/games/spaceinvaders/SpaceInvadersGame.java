39923be937f81cc7272f319a5f219a6b289f772a
Pavel Smirnov <smirnov@0x55.ru>
2021-02-25 16:22:20 +0300

    Игра Space Invaders (8/34)

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -2,6 +2,7 @@ package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.Color;
 import com.javarush.engine.cell.Game;
+import com.javarush.games.spaceinvaders.gameobjects.EnemyFleet;
 import com.javarush.games.spaceinvaders.gameobjects.Star;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private List<Star> stars;
+    private EnemyFleet enemyFleet;
 
     @Override
     public void initialize() {
@@ -23,12 +25,15 @@ public class SpaceInvadersGame extends Game {
 
 
     private void createGame() {
+        setTurnTimer(40);
         createStars();
+        enemyFleet = new EnemyFleet();
         drawScene();
     }
 
     private void drawScene() {
         drawField();
+        enemyFleet.draw(this);
     }
 
     private void drawField() {
@@ -47,5 +52,10 @@ public class SpaceInvadersGame extends Game {
         for(int i=0; i<8; i++){
             stars.add(new Star(getRandomNumber(WIDTH),getRandomNumber(HEIGHT)));
         }
+    }
+
+    @Override
+    public void onTurn(int step) {
+        drawScene();
     }
 }
--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.Game;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 import java.util.ArrayList;
@@ -8,13 +9,28 @@ import java.util.List;
 /**
  * @author pavelsmirnov
  */
-public class EnemyFleet{
+public class EnemyFleet {
     private final static int ROWS_COUNT = 3;
     private final static int COLUMNS_COUNT = 10;
     private final static int STEP = ShapeMatrix.ENEMY.length + 1;
     private List<EnemyShip> ships;
 
-    private void createShips(){
+    public EnemyFleet() {
+        createShips();
+    }
+
+    private void createShips() {
         ships = new ArrayList<>();
+        for (int x = 0; x < COLUMNS_COUNT; x++) {
+            for (int y = 0; y < ROWS_COUNT; y++) {
+                ships.add(new EnemyShip(x * STEP,y * STEP + 12));
+            }
+        }
+    }
+
+    public void draw(Game game){
+        for (EnemyShip eShip: ships) {
+            eShip.draw(game);
+        }
     }
 }
