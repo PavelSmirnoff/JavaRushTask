2c78c02706e49960f55a3739b1abde8179a1d63c
Pavel Smirnov <smirnov@0x55.ru>
2021-03-01 13:59:37 +0300

    Построй дерево(2)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,6 +2,8 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
 
 /* 
 Построй дерево(1)
@@ -11,11 +13,30 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public String get(int index) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int size() {
         return 0;
+    }
+
+    public String set(int index, String element){
+        throw new UnsupportedOperationException();
+    }
+    public void add(int index, String element){
+        throw new UnsupportedOperationException();
+    }
+    public String remove(int index){
+        throw new UnsupportedOperationException();
+    }
+    public List<String> subList(int fromIndex, int toIndex){
+        throw new UnsupportedOperationException();
+    }
+    protected void removeRange(int fromIndex, int toIndex){
+        throw new UnsupportedOperationException();
+    }
+    public boolean addAll(int index, Collection<? extends String> c){
+        throw new UnsupportedOperationException();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -6,4 +6,5 @@ public class Solution {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
     }
+
 }
