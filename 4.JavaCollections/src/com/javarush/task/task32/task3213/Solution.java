31ec4795c2cefba2e56a820e4a49e49e2a19db52
Pavel Smirnov <smirnov@0x55.ru>
2021-03-08 15:43:57 +0300

    Шифр Цезаря

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task32.task3213;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/* 
+Шифр Цезаря
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringReader reader = new StringReader("Khoor#Dpljr#&C,₷B'3");
+        System.out.println(decode(reader, -3));  //Hello Amigo #@)₴?$0
+    }
+
+    public static String decode(StringReader reader, int key) throws IOException {
+        if(reader == null) return "";
+        int num;
+        StringBuilder stringBuilder = new StringBuilder();
+        while((num = reader.read()) != -1){
+            stringBuilder.append((char) (num + key));
+        }
+        return stringBuilder.toString();
+    }
+}
