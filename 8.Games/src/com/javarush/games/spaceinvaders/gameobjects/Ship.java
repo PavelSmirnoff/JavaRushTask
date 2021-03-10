fcc66c783057b56781febf4fe15972d497024b10
Pavel Smirnov <smirnov@0x55.ru>
2021-03-10 20:25:17 +0300

    Игра Space Invaders (18/34)

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
@@ -1,9 +1,15 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author pavelsmirnov
  */
 public class Ship extends GameObject{
+    private List<int[][]> frames;
+    private int frameIndex;
     public boolean isAlive = true;
 
     public Ship(double x, double y) {
@@ -12,6 +18,9 @@ public class Ship extends GameObject{
 
     public void setStaticView(int[][] viewFrame){
         super.setMatrix(viewFrame);
+        frames = new ArrayList<int[][]>();
+        frames.add(viewFrame);
+        frameIndex = 0;
     }
 
     public Bullet fire(){
@@ -20,5 +29,12 @@ public class Ship extends GameObject{
 
     public void kill(){
         isAlive = false;
+    }
+
+    public void setAnimatedView(int[][]... viewFrames){
+        setMatrix(viewFrames[0]);
+        frames = Arrays.asList(viewFrames);
+        frameIndex = 0;
+
     }
 }
