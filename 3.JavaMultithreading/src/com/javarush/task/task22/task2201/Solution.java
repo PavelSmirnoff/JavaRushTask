d91e1a162c1f00ff61aa3b07f7537c4f21c02e67
Pavel Smirnov <smirnov@0x55.ru>
2021-03-23 19:38:44 +0300

    Строки нитей или строковые нити? Вот в чем вопрос

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2201/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task22.task2201;
+
+/* 
+Строки нитей или строковые нити? Вот в чем вопрос
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    public static final String FIRST_THREAD_NAME = "1#";
+    public static final String SECOND_THREAD_NAME = "2#";
+
+    private Thread thread1;
+    private Thread thread2;
+    private Thread thread3;
+
+    public Solution() {
+        initThreads();
+    }
+
+    protected void initThreads() {
+        this.thread1 = new Thread(new Task(this, "A\tB\tC\tD\tE\tF\tG\tH\tI"), FIRST_THREAD_NAME);
+        this.thread2 = new Thread(new Task(this, "J\tK\tL\tM\tN\tO\tP\tQ\tR\tS\tT\tU\tV\tW\tX\tY\tZ"), SECOND_THREAD_NAME);
+        this.thread3 = new Thread(new Task(this, "\t\t"), "3#");
+
+        Thread.setDefaultUncaughtExceptionHandler(new OurUncaughtExceptionHandler());
+
+        this.thread1.start();
+        this.thread2.start();
+        this.thread3.start();
+    }
+
+    public synchronized String getPartOfString(String string, String threadName) {
+
+        if (string == null)
+            switch (threadName) {
+                case "1#":
+                    throw new StringForFirstThreadTooShortException();
+                case "2#":
+                    throw new StringForSecondThreadTooShortException();
+                default:
+                    throw new RuntimeException();
+            }
+
+        int start = string.indexOf('\t');
+        int end = string.lastIndexOf('\t');
+
+        if ((end - start) < 2)
+            switch (threadName) {
+                case "1#":
+                    throw new StringForFirstThreadTooShortException();
+                case "2#":
+                    throw new StringForSecondThreadTooShortException();
+                default:
+                    throw new RuntimeException();
+            }
+
+        return string.substring(start+1,end);
+    }
+}
