6e7e96256415e99c843fd2c9b322f5fed1198d13
Pavel Smirnov <smirnov@0x55.ru>
2021-03-30 20:12:24 +0300

    Анонимность иногда так приятна

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Server.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task23.task2309.vo;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 30.03.2021
+ */
+public class Server extends NamedItem{
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/User.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task23.task2309.vo;
+
+import java.util.jar.Attributes;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 30.03.2021
+ */
+public class User extends NamedItem {
+}
