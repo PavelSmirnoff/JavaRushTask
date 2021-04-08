764c05d80196fe764bb4e8e0f666a4194c2463de
Pavel Smirnov <smirnov@0x55.ru>
2021-04-08 14:34:39 +0300

    Ипподром(1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Hippodrome game
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 08.04.2021
+ */
+public class Hippodrome {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Game object for Hippodrome game
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 08.04.2021
+ */
+public class Horse {
+}
