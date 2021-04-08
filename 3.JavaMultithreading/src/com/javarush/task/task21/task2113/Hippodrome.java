6c57b04667951f6fbb63067b4da8779ac335be36
Pavel Smirnov <smirnov@0x55.ru>
2021-04-08 14:41:10 +0300

    Ипподром(3)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -14,11 +14,15 @@ public class Hippodrome {
 
     private List<Horse> horses = new ArrayList<>();
 
-    public static void main(String[] args) {
-
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
     }
 
     public List<Horse> getHorses() {
         return horses;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
