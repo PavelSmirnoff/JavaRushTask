48e4826a25292628d0173e5555a0ad8e984b290c
Pavel Smirnov <smirnov@0x55.ru>
2021-04-14 20:52:17 +0300

    Машину на СТО не повезем

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task25.task2502;
+
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/* 
+Машину на СТО не повезем!
+*/
+
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws Exception {
+            //init wheels here
+            Set<Wheel> wheelSet = new HashSet<>(4);
+            String[] wheelNamesFromDb = loadWheelNamesFromDB();
+            if (wheelNamesFromDb.length != 4) throw new IllegalArgumentException();
+            for (String wheelName : wheelNamesFromDb) {
+                wheelSet.add(Wheel.valueOf(wheelName));
+            }
+            if (wheelSet.size() != 4) throw new IllegalArgumentException();
+            wheels = new ArrayList<>(wheelSet);
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
