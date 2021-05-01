283e521a1640b9cb9ac74a5c5f00d5ac880dcb14
Pavel Smirnov <smirnov@0x55.ru>
2021-05-01 21:55:16 +0300

    SpaceInvadersGame #30

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 /**
@@ -9,7 +10,7 @@ import com.javarush.games.spaceinvaders.ShapeMatrix;
  * @version 0.1
  * Created 01.05.2021
  */
-public class Boss extends EnemyShip{
+public class Boss extends EnemyShip {
 
     private int frameCount = 0;
 
@@ -19,8 +20,24 @@ public class Boss extends EnemyShip{
     }
 
     @Override
+    public Bullet fire() {
+        if (!isAlive) return null;
+        return (matrix == ShapeMatrix.BOSS_ANIMATION_FIRST) ?
+                new Bullet(x + 6, y + height, Direction.DOWN) :
+                new Bullet(x, y + height, Direction.DOWN);
+    }
+
+    @Override
     public void nextFrame() {
         frameCount++;
-        if(!isAlive||frameCount%10==0) super.nextFrame();
+        if (!isAlive || frameCount % 10 == 0) super.nextFrame();
+    }
+
+    @Override
+    public void kill() {
+        isAlive = false;
+        super.setAnimatedView(ShapeMatrix.KILL_BOSS_ANIMATION_FIRST,
+                ShapeMatrix.KILL_BOSS_ANIMATION_SECOND,
+                ShapeMatrix.KILL_BOSS_ANIMATION_THIRD);
     }
 }
