5a48af9cdb68e4cc59fcc37126a36026831f4196
Pavel Smirnov <smirnov@0x55.ru>
2021-05-02 20:32:32 +0300

    Arkanoid game start task

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 02.05.2021
+ */
+public class Arkanoid
+{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 02.05.2021
+ */
+public class Stand {
+}
