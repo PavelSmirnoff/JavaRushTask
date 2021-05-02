38213ca4a07be1fee47f1512ff230c2f174e8099
Pavel Smirnov <smirnov@0x55.ru>
2021-05-02 21:40:34 +0300

    Arkanoid #17

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -27,7 +27,7 @@ public class Ball extends BaseObject{
     }
 
     void checkRebound(int minx, int maxx, int miny, int maxy){
-
+        
     }
 
     public double getDx() {
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+    private double speed;
+    private double direction;
+
     @Override
     void draw(Canvas canvas) {
         
@@ -8,10 +11,36 @@ public class Stand extends BaseObject{
 
     @Override
     void move() {
-
+        x += (direction * speed);
     }
 
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public void moveLeft(){
+        direction = -1;
+    }
+
+    public void moveRight() {
+        direction = 1;
     }
 }
