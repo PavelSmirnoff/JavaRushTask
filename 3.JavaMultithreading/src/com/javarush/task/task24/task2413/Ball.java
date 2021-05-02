fdef1492d1bbd32fe04a7e11ca9a16a0eef19a98
Pavel Smirnov <smirnov@0x55.ru>
2021-05-02 21:58:54 +0300

    Arkanoid task Solved

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,11 +1,29 @@
 package com.javarush.task.task24.task2413;
 
-public class Ball extends BaseObject{
+/**
+ * Класс для шарика в игре
+ */
+public class Ball extends BaseObject {
+    // скорость
     private double speed;
+    // направление  (в градусах от 0 до 360)
     private double direction;
+
+    // текущее значение вектора движения (dx,dy)
     private double dx;
     private double dy;
+
+    // заморожен ли объект или может двигаться
     private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+
+        this.direction = direction;
+        this.speed = speed;
+
+        this.isFrozen = true;
+    }
 
     public double getSpeed() {
         return speed;
@@ -19,52 +37,80 @@ public class Ball extends BaseObject{
         return direction;
     }
 
-    public void setDirection(double direction) {
-        this.direction = direction;
-        double angle = Math.toRadians(direction);
-        dx = Math.cos(angle) * speed;
-        dy = -Math.sin(angle) * speed;
-    }
-
-    void checkRebound(int minx, int maxx, int miny, int maxy){
-        
-    }
-
     public double getDx() {
         return dx;
-    }
-
-    public void setDx(double dx) {
-        this.dx = dx;
     }
 
     public double getDy() {
         return dy;
     }
 
-    public void setDy(double dy) {
-        this.dy = dy;
+    /**
+     * Устанавливаем новое направление движения.
+     * Тут же вычисляем и новый вектор.
+     * Такой подход удобно использовать при отскоках от стен.
+     */
+    void setDirection(double direction) {
+        this.direction = direction;
+
+        double angle = Math.toRadians(direction);
+        dx = Math.cos(angle) * speed;
+        dy = -Math.sin(angle) * speed;
     }
 
+    /**
+     * Рисуем себя на "канвасе".
+     */
     @Override
     void draw(Canvas canvas) {
-        canvas.setPoint(x,y,'O');
+        canvas.setPoint(x, y, 'O');
     }
 
-    @Override
-    void move() {
-        if(isFrozen) return;
-        x+=dx;
-        y+=dy;
-    }
-    void start(){
-        isFrozen = false;
+    /**
+     * Двигаем себя на один шаг.
+     */
+    public void move() {
+        if (isFrozen) return;
+
+        x += dx;
+        y += dy;
+
+        checkRebound(1, Arkanoid.game.getWidth(), 1, Arkanoid.game.getHeight() + 5);
     }
 
-    public Ball(double x, double y, double speed, double direction) {
-        super(x, y, 1);
-        this.speed = speed;
-        this.direction = direction;
-        isFrozen = true;
+    /**
+     * Проверяем не улетел ли шарик за стенку.
+     * Если да - отражаем его.
+     */
+    void checkRebound(int minx, int maxx, int miny, int maxy) {
+        if (x < minx) {
+            x = minx + (minx - x);
+            dx = -dx;
+        }
+
+        if (x > maxx) {
+            x = maxx - (x - maxx);
+            dx = -dx;
+        }
+
+        if (y < miny) {
+            y = miny + (miny - y);
+            dy = -dy;
+        }
+
+        if (y > maxy) {
+            y = maxy - (y - maxy);
+            dy = -dy;
+        }
+    }
+
+    /**
+     * Запускам шарик.
+     * isFrozen = false.
+     * Пересчитываем вектор движения (dx,dy).
+     */
+    void start() {
+        this.setDirection(direction);
+        this.isFrozen = false;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,46 +1,64 @@
 package com.javarush.task.task24.task2413;
 
-public class Stand extends BaseObject{
-    private double speed;
-    private double direction;
+/**
+ * Подставка, с помощью которой отражаем мячик.
+ */
+public class Stand extends BaseObject {
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
 
-    @Override
-    void draw(Canvas canvas) {
-        
-    }
-
-    @Override
-    void move() {
-        x += (direction * speed);
-    }
+    //скорость
+    private double speed = 1;
+    //направление (-1 влево, +1 вправо)
+    private double direction = 0;
 
     public Stand(double x, double y) {
         super(x, y, 3);
-        speed = 1;
-        direction = 0;
+    }
+
+    /**
+     * Метод передвигает подставку в соответствии с текущим значением direction.
+     */
+    void move() {
+        double dx = speed * direction;
+        x = x + dx;
+
+        checkBorders(radius, Arkanoid.game.getWidth() - radius + 1, 1, Arkanoid.game.getHeight() + 1);
+    }
+
+    /**
+     * direction устанавливается равным -1
+     */
+    void moveLeft() {
+        direction = -1;
+    }
+
+    /**
+     * direction устанавливается равным +1
+     */
+    void moveRight() {
+        direction = 1;
     }
 
     public double getSpeed() {
         return speed;
     }
 
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
     public double getDirection() {
         return direction;
     }
 
-    public void setDirection(double direction) {
-        this.direction = direction;
-    }
-
-    public void moveLeft(){
-        direction = -1;
-    }
-
-    public void moveRight() {
-        direction = 1;
+    /**
+     * Отрисовываем себя на холсте
+     */
+    @Override
+    void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
     }
 }
