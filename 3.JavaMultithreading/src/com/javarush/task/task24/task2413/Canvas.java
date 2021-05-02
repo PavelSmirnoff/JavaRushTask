2590e8a77fdeedef37ccb2d1c54644d71c2c1662
Pavel Smirnov <smirnov@0x55.ru>
2021-05-02 21:10:55 +0300

    Arkanoid #10

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,4 +1,52 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    void setPoint(double x, double y, char c) {
+        int dx = (int) Math.round(x);
+        int dy = (int) Math.round(y);
+        if (x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length) return;
+        matrix[dy][dx] = c;
+    }
+
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) setPoint(x + j, y + i, c);
+            }
+        }
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -3,7 +3,7 @@ package com.javarush.task.task24.task2413;
 public class Stand extends BaseObject{
     @Override
     void draw(Canvas canvas) {
-
+        
     }
 
     @Override
