c6edcddc5438cc27c98c6aafeeb67ac09db41fd0
Pavel Smirnov <smirnov@0x55.ru>
2021-05-07 21:15:46 +0300

    Switch для нитей

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task25.task2504;
+
+/* 
+Switch для нитей
+*/
+
+public class Solution {
+    public static void processThreads(Thread... threads) {
+        //implement this method - реализуйте этот метод
+        for (Thread thread : threads) {
+            switch (thread.getState()) {
+                case NEW:
+                    thread.start();
+                    break;
+                case TIMED_WAITING:
+                case WAITING:
+                case BLOCKED:
+                    thread.interrupt();
+                    break;
+                case RUNNABLE:
+                    thread.isInterrupted();
+                    break;
+                case TERMINATED:
+                    System.out.println(thread.getPriority());
+                    break;
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
