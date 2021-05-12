29e5f8481675a22fd7543e6d7addb1d5ef3efd5d
Pavel Smirnov <smirnov@0x55.ru>
2021-05-12 14:18:43 +0300

    Tetris #5

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,8 +3,26 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+    public static Tetris game;
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
 
     public static void main(String[] args) {
+            game = new Tetris();
+            game.run();
+    }
 
+    public void run(){
+
+    }
+
+    public void step(){
+        
     }
 }
