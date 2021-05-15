8a0b6992b569625791c0a98afdb19360e3ec01a5
Pavel Smirnov <smirnov@0x55.ru>
2021-05-15 22:14:23 +0300

    Collections & Generics

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+/* 
+Collections & Generics
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        //напишите тут ваш код
+        return new ArrayList<>(Arrays.asList(elements));
+    }
+
+    public static <T> HashSet<T> newHashSet(T... elements) {
+        //напишите тут ваш код
+        return new HashSet<>(Arrays.asList(elements));
+    }
+
+    public static <K, V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
+        //напишите тут ваш код
+        if (keys.size() != values.size()) throw new IllegalArgumentException();
+
+        HashMap<K, V> map = new HashMap<>();
+        for (int i = 0; i < keys.size(); i++) {
+            map.put(keys.get(i), values.get(i));
+        }
+
+        return map;
+    }
+}
