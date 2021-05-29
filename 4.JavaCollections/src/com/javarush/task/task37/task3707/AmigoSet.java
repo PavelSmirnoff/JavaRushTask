019453a385c4c46ac965adecf09a4508ee2dd56b
Pavel Smirnov <smirnov@0x55.ru>
2021-05-29 20:33:54 +0300

    AmigoSet (1)

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,59 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    @Override
+    public Spliterator spliterator() {
+        return Set.super.spliterator();
+    }
+
+    @Override
+    public boolean removeIf(Predicate filter) {
+        return super.removeIf(filter);
+    }
+
+    @Override
+    public Stream stream() {
+        return super.stream();
+    }
+
+    @Override
+    public Stream parallelStream() {
+        return super.parallelStream();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        super.forEach(action);
+    }
+}
