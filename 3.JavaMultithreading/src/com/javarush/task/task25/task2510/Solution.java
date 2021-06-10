5c1b28a01bb442dab96520ea736a45184e8106ce
Pavel Smirnov <smirnov@0x55.ru>
2021-06-10 21:27:37 +0300

    Поживем - увидим

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task25.task2510;
+
+/* 
+Поживем - увидим
+*/
+
+
+public class Solution extends Thread {
+
+    public Solution() {
+        this.setUncaughtExceptionHandler((t, e) -> {
+            String message;
+            if (e instanceof Error) message = "Нельзя дальше работать";
+            else if (e instanceof Exception) message = "Надо обработать";
+            else message = "Поживем - увидим";
+            System.out.println(message);
+        });
+    }
+
+    public static void main(String[] args) {
+    }
+}
