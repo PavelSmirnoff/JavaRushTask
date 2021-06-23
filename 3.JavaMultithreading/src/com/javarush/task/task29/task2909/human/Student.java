a888dada6ea3420b773c244811866aa0cfec5db5
Pavel Smirnov <smirnov@0x55.ru>
2021-06-23 21:04:10 +0300

    Рефакторинг (7)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -40,12 +40,16 @@ public class Student extends UniversityPerson {
 //        System.out.println("Студент: " + name);
 //    }
 
-    public void incAverageGradeBy01() {
-        averageGrade += 0.1;
-    }
+//    public void incAverageGradeBy01() {
+//        averageGrade += 0.1;
+//    }
+//
+//    public void incAverageGradeBy02() {
+//        averageGrade += 0.2;
+//    }
 
-    public void incAverageGradeBy02() {
-        averageGrade += 0.2;
+    public void incAverageGrade(double delta){
+        averageGrade += delta;
     }
 
 //    public void setValue(String name, double value) {
@@ -66,12 +70,12 @@ public class Student extends UniversityPerson {
         course = value;
     }
 
-    public void setBeginningOfSession(int day, int month, int year) {
-        beginningOfSession = new Date(year, month, day);
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
     }
 
-    public void setEndOfSession(int day, int month, int year) {
-        endOfSession = new Date(year, month, day);
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
     }
 
     public double getAverageGrade() {
