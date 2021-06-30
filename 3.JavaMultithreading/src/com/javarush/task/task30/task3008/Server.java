c6baa00884e891c38a04ccc5bd2d56a07465e005
Pavel Smirnov <smirnov@0x55.ru>
2021-06-30 21:49:37 +0300

    Чат (6)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,8 +1,28 @@
 package com.javarush.task.task30.task3008;
 
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ConsoleHelper.writeMessage("Введите порт:");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Сервер запущен.");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
