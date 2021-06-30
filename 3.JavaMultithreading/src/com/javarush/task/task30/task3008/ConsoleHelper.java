3d198415267e5a0907979cc434f3a1c598930a99
Pavel Smirnov <smirnov@0x55.ru>
2021-06-30 20:31:59 +0300

    Чат (2)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,33 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    static String readString() {
+        while (true) {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString());
+            } catch (NumberFormatException e) {
+                System.out.println("ППроизошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
 }
