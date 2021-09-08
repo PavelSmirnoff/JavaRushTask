be0dad009f817bedb26bbab9b5ea8fba060f6179
PavelSmirnoff <smirnov@0x55.ru>
2021-09-08 22:32:46 +0300

    Shortener#8

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/OurHashMapStorageStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/OurHashMapStorageStrategy.java
@@ -18,52 +18,94 @@ public class OurHashMapStorageStrategy implements StorageStrategy {
         return hash & (length - 1);
     }
 
-    private Entry getEntry(Long key) {
-        return null;
-    }
-
-    private void resize(int newCapacity) {
-
-    }
-
-    private void transfer(Entry[] newTable) {
-
-    }
-
-    private void addEntry(int hash, Long key, String value, int bucketIndex){
-
-    }
-
-    private void createEntry(int hash, Long key, String value, int bucketIndex){
-
-    }
-
-    @Override
-    public boolean containsKey(Long key) {
-        for (Entry entry :table) {
-            if(entry.getKey().equals(key)) return true;
+    final Entry getEntry(Long key) {
+        if (size == 0) {
+            return null;
         }
-        return false;
+
+        int hash = hash(key);
+        int index = indexFor(hash, table.length);
+        for (Entry entry = table[index]; entry != null; entry = entry.next) {
+            if (key.equals(entry.key)) {
+                return entry;
+            }
+        }
+        return null;
     }
 
-    @Override
-    public boolean containsValue(String value) {
-
-        return false;
-    }
-
-    @Override
     public void put(Long key, String value) {
-
+        int hash = hash(key);
+        int index = indexFor(hash, table.length);
+        for (Entry e = table[index]; e != null; e = e.next) {
+            if (key.equals(e.key)) {
+                e.value = value;
+                return;
+            }
+        }
+        addEntry(hash, key, value, index);
     }
 
-    @Override
-    public Long getKey(String value) {
-        return null;
+    void resize(int newCapacity) {
+        Entry[] newTable = new Entry[newCapacity];
+        transfer(newTable);
+        table = newTable;
+        threshold = (int) (newCapacity * loadFactor);
     }
 
-    @Override
+    void transfer(Entry[] newTable) {
+        int newCapacity = newTable.length;
+        for (Entry e : table) {
+            while (e != null) {
+                Entry next = e.next;
+                int indexInNewTable = indexFor(hash(e.getKey()), newCapacity);
+                e.next = newTable[indexInNewTable];
+                newTable[indexInNewTable] = e;
+                e = next;
+            }
+        }
+    }
+
+    public boolean containsValue(String value) {
+        for (Entry tableElement : table)
+            for (Entry e = tableElement; e != null; e = e.next)
+                if (value.equals(e.value))
+                    return true;
+        return false;
+    }
+
     public String getValue(Long key) {
+        Entry entry = getEntry(key);
+        if (entry != null)
+            return entry.getValue();
+
         return null;
+    }
+
+    public boolean containsKey(Long key) {
+        return getEntry(key) != null;
+    }
+
+    public Long getKey(String value) {
+        for (Entry tableElement : table)
+            for (Entry e = tableElement; e != null; e = e.next)
+                if (value.equals(e.value))
+                    return e.getKey();
+        return null;
+    }
+
+    void addEntry(int hash, Long key, String value, int bucketIndex) {
+        if ((size >= threshold)) {
+            resize(2 * table.length);
+            hash = hash(key);
+            bucketIndex = indexFor(hash, table.length);
+        }
+
+        createEntry(hash, key, value, bucketIndex);
+    }
+
+    void createEntry(int hash, Long key, String value, int bucketIndex) {
+        Entry e = table[bucketIndex];
+        table[bucketIndex] = new Entry(hash, key, value, e);
+        size++;
     }
 }
